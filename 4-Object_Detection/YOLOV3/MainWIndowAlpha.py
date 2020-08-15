3ba353cc85275574291d382e1a29bb4df4f3d955
santoo32 <santiagot.roa@gmail.com>
2020-08-15 18:21:58 -0300

    Dev - Code comment

--- 4-Object_Detection/YOLOV3/MainWIndowAlpha.py
+++ 4-Object_Detection/YOLOV3/MainWIndowAlpha.py
@@ -12,27 +12,29 @@ class MyWindow(QMainWindow):
         super(MyWindow,self).__init__()
         self.initUI()
     
+    # Start yolov3 detection
     def start(self):
         message = vd.startDetection(self, self.spinBox.value(), self.filePath.text())
         print(message)
 
     def valuechange(self):
-        # print(self.spinBox.value())
+        print(self.spinBox.value())
 
     def initUI(self):
-        # self.setGeometry(200, 200, 300, 300)
+        # Main window
         self.resize(926, 600)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("img_34913.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setWindowIcon(icon)
         
-        self.label = QtWidgets.QLabel(self)
-        self.label.setGeometry(QtCore.QRect(350, 0, 141, 51))
-
-        self.label.setTabletTracking(False)
-        self.label.setWordWrap(True)
-        self.label.setObjectName("label")
+        # Main window title
+        self.title = QtWidgets.QLabel(self)
+        self.title.setGeometry(QtCore.QRect(350, 0, 141, 51))
+        self.title.setTabletTracking(False)
+        self.title.setWordWrap(True)
+        self.title.setObjectName("label")
         
+        # Start detection button
         self.runButton = QtWidgets.QPushButton(self)
         self.runButton.setGeometry(QtCore.QRect(750, 550, 121, 41))
         icon1 = QtGui.QIcon()
@@ -42,34 +44,29 @@ class MyWindow(QMainWindow):
         self.runButton.setFlat(False)
         self.runButton.setObjectName("runButton")
         self.runButton.clicked.connect(self.start)
-
-        self.label_3 = QtWidgets.QLabel(self)
-        self.label_3.setGeometry(QtCore.QRect(10, 10, 41, 31))
-        self.label_3.setAutoFillBackground(False)
-        self.label_3.setText("")
-        self.label_3.setPixmap(QtGui.QPixmap("../Downloads/cctv.png"))
-        self.label_3.setScaledContents(True)
-        self.label_3.setObjectName("label_3")
         
+        # Separator line
         self.line = QtWidgets.QFrame(self)
         self.line.setGeometry(QtCore.QRect(700, 30, 21, 561))
         self.line.setFrameShape(QtWidgets.QFrame.VLine)
         self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line.setObjectName("line")
         
+        # Spinner box title
         self.label_2 = QtWidgets.QLabel(self)
         self.label_2.setGeometry(QtCore.QRect(720, 230, 81, 16))
         self.label_2.setObjectName("label_2")
-        
+        # Confidence spinbox
         self.spinBox = QtWidgets.QSpinBox(self)
         self.spinBox.setGeometry(QtCore.QRect(720, 250, 42, 22))
         self.spinBox.setMinimum(1)
         self.spinBox.setObjectName("spinBox")
         self.spinBox.valueChanged.connect(self.valuechange)
         
-        self.label_4 = QtWidgets.QLabel(self)
-        self.label_4.setGeometry(QtCore.QRect(10, 60, 661, 521))
-        self.label_4.setObjectName("label_4")
+        # Big main label to display an image
+        self.imageDisplay = QtWidgets.QLabel(self)
+        self.imageDisplay.setGeometry(QtCore.QRect(10, 60, 661, 521))
+        self.imageDisplay.setObjectName("label_4")
 
         #Video file input
         self.label_5 = QtWidgets.QLabel(self)
@@ -84,50 +81,50 @@ class MyWindow(QMainWindow):
         self.examineButton.setText("Examine")
         self.examineButton.clicked.connect(self.getfiles)
 
+        # Values initialization
         self.setWindowTitle("Orwell surveillance")
-        self.label.setText("PFC - 2020 - V0.2")
+        self.title.setText("PFC - 2020 - V0.2")
         self.runButton.setText("Run")
         self.label_2.setText("Confidence")
         self.label_5.setText("Video file")
-        # Empty label to display image
-        self.label_4.setText("")
+        self.imageDisplay.setText("")
 
         
+        # Custom color palete, currently unused
 
-        dark_palette = QtGui.QPalette()
+        # dark_palette = QtGui.QPalette()
 
-        dark_palette.setColor(QPalette.Window, QColor(235, 235, 235))
-        dark_palette.setColor(QPalette.WindowText, QColor(235, 235, 235))
-        dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
-        dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-        dark_palette.setColor(QPalette.ToolTipBase, QColor('white'))
-        dark_palette.setColor(QPalette.ToolTipText, QColor('white'))
-        dark_palette.setColor(QPalette.Text, QColor(255, 255, 255))
-        dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
-        dark_palette.setColor(QPalette.ButtonText, QColor('white'))
-        dark_palette.setColor(QPalette.BrightText, QColor('red'))
-        dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
-        dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-        dark_palette.setColor(QPalette.HighlightedText, QColor('black'))
+        # dark_palette.setColor(QPalette.Window, QColor(235, 235, 235))
+        # dark_palette.setColor(QPalette.WindowText, QColor(235, 235, 235))
+        # dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+        # dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+        # dark_palette.setColor(QPalette.ToolTipBase, QColor('white'))
+        # dark_palette.setColor(QPalette.ToolTipText, QColor('white'))
+        # dark_palette.setColor(QPalette.Text, QColor(255, 255, 255))
+        # dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+        # dark_palette.setColor(QPalette.ButtonText, QColor('white'))
+        # dark_palette.setColor(QPalette.BrightText, QColor('red'))
+        # dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+        # dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+        # dark_palette.setColor(QPalette.HighlightedText, QColor('black'))
 
-        self.setPalette(dark_palette)
+        # self.setPalette(dark_palette)
 
-        self.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+        # self.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
 
+    # Get files from a path
     def getfiles(self):
       dlg = QFileDialog(self)
-      # dlg.setFileMode(QFileDialog.AnyFile)
+      # Set available file types 
       dlg.setNameFilters(["Videos (*.mp4 *.mkv)"])
 
-      #selected_filter = "Images (*.png *.xpm *.jpg)"
-      #dlg.setFilter("Text files (*.png)")
-      #filenames = QStringList()
 		
       if dlg.exec_():
          filenames = dlg.selectedFiles()
          f = open(filenames[0], 'r')
 			
          with f:
+            # Update input text field and label 
             self.filePath.setText(f.name)
             self.label_4.setPixmap(QtGui.QPixmap(f.name))
 
